598d8ad4d82efc01168ed97f31232af2b756af82
Aash1236 <ashutoshfase1028@gmil.com>
2024-10-12 23:09:04 +0530

    initial

--- script.js
+++ script.js
@@ -2,3 +2,45 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#div'),
     smooth: true
 });
+
+// var a = document.querySelector("#elem1")
+// console.log(a)
+
+
+function page4Animation() {
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter", function () {
+        fixed.style.display = "block"
+    })
+    elemC.addEventListener("mouseleave", function () {
+        fixed.style.display = "none"
+    })
+
+    // var elem1 = document.querySelector("#elem1")
+// elem1.addEventListener("mouseenter", function(){
+//     var image = elem1.getAttribute("data-image")
+//     fixed.style.backgroundImage = `url(${image})`
+// })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+
+
+function swiperAnimation() {
+    var swiper = new swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+        
+      });
+}
+
+swiperAnimation()
+page4Animation()

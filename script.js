7b8e5828778ca4d06dbf0dd2ef18a6b25d7f73de
Aash1236 <ashutoshfase1028@gmil.com>
2024-10-13 16:05:04 +0530

    loader

--- script.js
+++ script.js
@@ -50,18 +50,11 @@ page4Animation()
 document.addEventListener('DOMContentLoaded', function() {
     var defaultImage = document.querySelector('.left-part a').getAttribute('data-image');
     
-    // Set the default image on page load
     document.getElementById('display-image').setAttribute('src', defaultImage);
-  
-    // Add click event listeners to all anchor tags
     document.querySelectorAll('.left-part a').forEach(function(anchor) {
       anchor.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent default anchor behavior
-  
-        // Get the data-image attribute from the clicked anchor
+        event.preventDefault();
         var newImage = this.getAttribute('data-image');
-  
-        // Set the src of the right-part image
         document.getElementById('display-image').setAttribute('src', newImage);
       });
     });
@@ -81,3 +74,34 @@ function swiperAnimation(){
       });
 }
 swiperAnimation()
+
+function menuAnimation() {
+
+  var menu = document.querySelector("nav h3")
+  var full = document.querySelector("#full-scr")
+  var navimg = document.querySelector("nav img")
+  var flag = 0
+  menu.addEventListener("click", function () {
+    if(flag == 0) {
+      full.style.top = 0
+      navimg.style.opacity = 0
+      flag = 1
+    }else{
+      full.style.top = "-100%"
+      navimg.style.opacity = 1
+      flag = 0
+    }
+  })
+}
+
+menuAnimation()
+
+function loaderAnimation (){
+
+  var loader = document.querySelector("#loader")
+  setTimeout(function(){
+    loader.style.top = "-100%"
+  },4200)
+}
+
+loaderAnimation()
